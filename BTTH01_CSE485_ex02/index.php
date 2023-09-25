2e8ad03e46240af4dcb399762843af32300de1b6
vanh1302 <118183970+vanh1302@users.noreply.github.com>
2023-09-25 11:35:40 +0700

    up

---
+++ BTTH01_CSE485_ex02/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"> 
+</head>
+<body>
+    <div class="container">
+        <div class = "header">
+            <div class ="header_left d-flex">
+                <img src="musicforlife.png">
+                <h5>TRANG CHỦ</h5>
+                <h5>ĐĂNG NHẬP</h5>
+            <div class = "header_right">
+
+            </div>
+            </div>
+
+        </div>
+
+        
+    </div>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
