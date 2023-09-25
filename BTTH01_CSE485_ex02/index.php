57f24627e871f9b84249c88775346e29d95c5da7
vanh1302 <118183970+vanh1302@users.noreply.github.com>
2023-09-25 23:20:00 +0700

    up

--- BTTH01_CSE485_ex02/index.php
+++ BTTH01_CSE485_ex02/index.php
@@ -1,31 +1,51 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"> 
-</head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 <body>
-    <div class="container">
-        <div class = "header">
-            <div class ="header_left d-flex">
-                <img src="musicforlife.png">
-                <h5>TRANG CHỦ</h5>
-                <h5>ĐĂNG NHẬP</h5>
-            <div class = "header_right">
-
+    <div class="container-fluid vh-100">
+        <div class="w-100 banner">
+            <a href="#">
+                <img src="musicforlife.jpg" class="w-100">
+            </a>
+        </div>
+        <div class="main-content my-3">
+            <p class="text-uppercase text-center fw-medium text-primary fs-5 mb-3">top bài hát yêu thích</p>
+            <div class="mx-2">
+                <ul class="row gap-1 flex-nowrap">
+                    <li class="col-3 border">
+                        <a href="">
+                            <img src="banner.png" class="w-100" alt="default">
+                            <p class="text-center my-2">Lòng mẹ</p>
+                        </a>
+                    </li>
+                    <li class="col-3 border">
+                        <a href="">
+                            <img src="longme.jpg" class="w-100" alt="default">
+                            <p class="text-center my-2">Lòng mẹ</p>
+                        </a>
+                    </li>
+                    <li class="col-3 border">
+                        <a href="">
+                            <img src="longme.jpg" class="w-100" alt="default">
+                            <p class="text-center my-2">Lòng mẹ</p>
+                        </a>
+                    </li>
+                    <li class="col-3 border">
+                        <a href="">
+                            <img src="longme.jpg" class="w-100" alt="default">
+                            <p class="text-center my-2">Lòng mẹ</p>
+                        </a>
+                    </li>
+                </ul>
             </div>
-            </div>
-
         </div>
 
-        
     </div>
-
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
